7ff6eacfed067db6311fd3f94a2a7500d1fc1d72
Chris Hughes <chris@speedycomputing.com>
2018-11-08 14:48:53 -0700

    Migrate to android x, disable backup

--- app/src/main/java/me/chrishughes/meetupeventslight/EventFragment.java
+++ app/src/main/java/me/chrishughes/meetupeventslight/EventFragment.java
@@ -1,13 +1,13 @@
 package me.chrishughes.meetupeventslight;
 
-import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
+import static androidx.recyclerview.widget.DividerItemDecoration.VERTICAL;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/me/chrishughes/meetupeventslight/EventsFragment.java
+++ app/src/main/java/me/chrishughes/meetupeventslight/EventsFragment.java
@@ -1,17 +1,17 @@
 package me.chrishughes.meetupeventslight;
 
-import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
+import static androidx.recyclerview.widget.DividerItemDecoration.VERTICAL;
 import static me.chrishughes.meetupeventslight.EventFragment.EVENT_ID;
 import static me.chrishughes.meetupeventslight.EventFragment.URL_NAME;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
